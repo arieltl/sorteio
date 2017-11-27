ed6ea88c51fcc1dcc51c82769fbf97e8cee9e2a2
alessandra leventhal <alessandraleventhal@macbook-pro-de-alessandra.local>
2017-11-27 16:25:11 -0200

    Initial Commit

---
+++ sorteio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sorteio
+//
+//  Created by alessandra leventhal on 27/11/2017.
+//  Copyright © 2017 ariel leventhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
